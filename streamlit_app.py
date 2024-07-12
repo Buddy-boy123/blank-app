c5edde550aeabcbb3f07f26bca281c121436ac4a
Buddy-boy123 <sudipgajurel23@gmail.com>
2024-07-13 00:30:14 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 I'm sick of all this")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.slider('Hey wanna slide?',1,11000,1100)
